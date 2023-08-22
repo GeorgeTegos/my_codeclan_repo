c02760119e706174653d2bbcc87ff4e1caabd17e
George Tegos <george.tegos.22@gmail.com>
2023-08-22 09:18:21 +0100

    update 1

--- 3_todo_list/todo-list/todo-list/src/components/Task.jsx
+++ 3_todo_list/todo-list/todo-list/src/components/Task.jsx
@@ -1,14 +1,8 @@
 function Task({task}){
     return (
         <>
-        <h3> Name of the task </h3>
-        {task.name}
-        <br />
-        <h3> Priority </h3>
-        {task.priority == 'high'? <div className="high-priority">{task.priority} </div>
-        : <div className="low-priority">{task.priority}</div>
-    }
-        
+        {task.priority == 'high'? <div className="high-priority">{task.name} </div>
+        : <div className="low-priority">{task.name}</div>}  
         <hr />
         </>
     )
