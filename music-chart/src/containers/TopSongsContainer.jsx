84a8bfdb8235d949b4d0251fb5b4993b37709ab7
George Tegos <george.tegos.22@gmail.com>
2023-08-24 09:53:46 +0100

    Async issue

--- music-chart/src/containers/TopSongsContainer.jsx
+++ music-chart/src/containers/TopSongsContainer.jsx
@@ -1,83 +1,93 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 function TopSongsContainer() {
-    const [data,setData] = useState([])
-    const [songsList,setSongsList] = useState([])
-    const [moreSongsInfo,setMoreSongsInfo] = useState([])
-    const [genreSelect,setGenreSelect]= useState("")
-    const [genres,setGenres]= useState([])
+    const [data, setData] = useState([]);
+    const [songsList, setSongsList] = useState([]);
+    const [moreSongsInfo, setMoreSongsInfo] = useState([]);
+    const [genreSelect, setGenreSelect] = useState("");
+    const [genresOptions,setGenresOptions] = useState("")
+    const [genres, setGenres] = useState([
+        {name: "All", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"},
+        {name: "Rock", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"},
+        {name: "Dance", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
+        {name: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"}
+    ]);
 
-
-    useEffect( () =>{
+    
+    useEffect(() => {
         fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
-        .then((res)=> res.json())
+        .then((res) => res.json())
         .then((data) => {
             const array = data.feed.entry
             setData(array)
+            setGenresOptions(genres.map((item,index) => <option key={index}>{item.name}</option>))
         })
-    }, [] )
+    }, []);
+
+    useEffect(() => async () => {
+        
+        console.log(genreSelect)
+        let promise
+        {genreSelect == "All"? promise = fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json"):
+        genreSelect == "Rock" ? promise = fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"):
+        genreSelect == "Dance" ? promise = fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"):
+        genreSelect == "Country" ? promise = fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"):
+        console.log("Error here")
+        }
+        promise.then((res)=> res.json())
+        .then((data)=>{
+            const array = data.feed.entry
+            setData(array)
+        })
+
+    }, [genreSelect]);
 
     const showSongs = () => {
-        setMoreSongsInfo([])
-        setSongsList(data.map((song, index)=> 
-        <li key={index} className="song-list-item">{song.title.label}</li>))
-    }
+        setMoreSongsInfo([]);
+        setSongsList(data.map((song, index) => (
+            <li key={index} className="song-list-item">{song.title.label}</li>
+        )));
+    };
 
-    const showMoreSongsInfo = () =>{
-        setSongsList([])
-        setMoreSongsInfo(data.map((song, index)=>{
-            const { "im:name":name, "im:artist":artist , category, "im:image":image} = song
+    const showMoreSongsInfo = () => {
+        setSongsList([]);
+        setMoreSongsInfo(data.map((song, index) => {
+            const { "im:name": name, "im:artist": artist, category, "im:image": image } = song;
             return (
-            <li key={index} className="more-info-list-item"> 
-                Song: {name.label} <br /> 
-                Artist: {artist.label} <br /> 
-                Category: {category.attributes.term} <br />
-                <img src={image[0].label} alt="" />
-                
-            </li>
-            )
-        } ))
-    }
+                <li key={index} className="more-info-list-item">
+                    Song: {name.label} <br />
+                    Artist: {artist.label} <br />
+                    Category: {category.attributes.term} <br />
+                    <img src={image[0].label} alt="" />
+                </li>
+            );
+        }));
+    };  
 
-    const genresArray = () =>{
-        setGenres(
-            data.map((genre) => {
-            const { category } = genre
-            return category.attributes.term})
-            .filter((eachGenre,pos,array)=> 
-            array.indexOf(eachGenre) == pos)
-            )
-        }
-        
-
-   
+    
 
 
-  return (
-    <>   
-        
-        <button onClick={showSongs}>Show Top 20 Songs</button>
-        <button onClick={showMoreSongsInfo}> More Infos </button>
-        <button onClick={genresArray}>Genres</button>
-       
- 
-        <ol>
-            {songsList}
-            {moreSongsInfo}
-        <label htmlFor="genres">
-        <select>
-            {genres.map((genre,index)=> {
-                return (
-                <option key={index}> {genre} </option>)
-                })}
-        </select>
-        </label>
-            
-        </ol>
 
-    </>
+    return (
+        <>   
+            <button onClick={showSongs}>Show Top 20 Songs</button>
+            <button onClick={showMoreSongsInfo}> More Infos </button>
 
-)
+            <label htmlFor="genre">Change Genre
+            <select onChange={(e)=> setGenreSelect(e.target.value)}>
+               {genresOptions}
+            </select>
+            </label> 
+
+            <ol>
+                {songsList}
+                {moreSongsInfo}
+            </ol>
+
+
+
+        </>
+    );
 }
 
-export default TopSongsContainer
+export default TopSongsContainer;
